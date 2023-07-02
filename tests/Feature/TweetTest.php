48bd7732d539412e55df21010569d67e3e4dacb3
Luan Araujo <luaanscar@gmail.com>
2023-07-02 12:17:44 -0300

    should make sure that only authenticated users can tweet

--- tests/Feature/TweetTest.php
+++ tests/Feature/TweetTest.php
@@ -4,24 +4,44 @@ use App\Http\Livewire\Tweet\Create;
 use App\Models\Tweet;
 use App\Models\User;
 
+use function Illuminate\Testing\Concerns;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Livewire\livewire;
 
-it('should be able create a tweet', function(){
+it('should be able create a tweet', function () {
     $user = User::factory()->create();
 
     actingAs($user);
 
     livewire(Create::class)
-    ->set('body', 'This is my first tweet')
-    ->call('tweet')
-    ->assertEmitted('tweet::created');
+        ->set('body', 'This is my first tweet')
+        ->call('tweet')
+        ->assertEmitted('tweet::created');
 
     assertDatabaseCount('tweets', 1);
 
     expect(Tweet::first())
-    ->body->toBe('This is my first tweet')
-    ->created_by->toBe($user->id);
+        ->body->toBe('This is my first tweet')
+        ->created_by->toBe($user->id);
 });
 
+it('should make sure that only authenticated users can tweet', function () {
+    livewire(Create::class)
+        ->set('body', 'This is my first tweet')
+        ->call('tweet')
+        ->assertForbidden();
+
+    actingAs(User::factory()->create());
+
+    livewire(Create::class)
+        ->set('body', 'This is my first tweet')
+        ->call('tweet')
+        ->assertEmitted('tweet::created');
+});
+
+
+todo('body is required');
+todo('the tweet body should have a max length of 140 characters');
+todo('should show the tweet on the timeline');
+todo('should set body as null after tweeting');
