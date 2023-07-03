74a5badb78e63d04d08ddee2b8390cf0e9e0bde6
Luan Araujo <luaanscar@gmail.com>
2023-07-02 23:24:45 -0300

    adding infinity scroll to our timeline

--- resources/views/livewire/timeline.blade.php
+++ resources/views/livewire/timeline.blade.php
@@ -1,6 +1,27 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
-    @foreach ($tweets as $tweet)
-    {{$tweet->body}}
+<div class="mt-10 text-white text-lg w-full">
+
+    @foreach($tweets as $tweet)
+    <div>
+        {{ $loop->index }} · {{ $tweet->body }}
+    </div>
     @endforeach
+
+    <div class="bg-blue-400 h-10 w-10" x-data="{
+            infinityScroll() {
+                const observer = new IntersectionObserver((items) => {
+                    items.forEach((item) => {
+                        if(item.isIntersecting) {
+                        console.log(item)
+                            @this.loadMore();
+                        }
+                    })
+                }, {
+                    threshold: 0.5, // 0 ... 1
+                    rootMargin: '100px'
+                })
+                observer.observe(this.$el)
+            }
+        }" x-init="infinityScroll()">
+
+    </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    Auth::login(User::first());
+
+    return view('dashboard');
 });
+
+
+Route::view('twitter', 'twitter')->name('twitter');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +26,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
